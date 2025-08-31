67fd666911e3c26b09d30ca90db8b83d42740f5b
KAMRUL345 <br7100@gmail.com>
2025-08-31 07:50:51 +0600

    simple change

--- routes/api.js
+++ routes/api.js
@@ -7,5 +7,12 @@ import AuthMiddleware from "../app/middlewares/AuthMiddleware.js";
 router.post("/register", UsersController.registerController);
 router.post("/login", UsersController.loginController);
 router.get("/read-user", AuthMiddleware, UsersController.readUserController);
-
+router.get("/", (req, res) => {
+  res.send("API is running...");
+});
 export default router;
+
+
+//to run this file use command "nodemon routes/api.js"
+// or "node routes/api.js" 
+//to run this file use command "node app.js"
